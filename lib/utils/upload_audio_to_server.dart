aa826555e31b4d2bdf4e1275ab30abd9aba3c8f0
Rishi Banerjee <rishieric91@gmail.com>
2019-03-02 19:09:49 +0530

    AudioUpload to REST API partially done

---
+++ lib/utils/upload_audio_to_server.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+
+void readAudioFile(String filePath, String apiUrl) async {
+  var config = File(filePath);
+
+  var audioFile = await config.readAsBytes();
+  File _audioFile; // TODO
+  Dio dio = Dio();
+  FormData formData = FormData(); // just like JS
+  formData.add("audio", UploadFileInfo(_audioFile, basename(filePath)));
+  dio
+      .post(
+        apiUrl,
+        data: formData,
+        options: Options(
+            method: 'POST',
+            responseType: ResponseType.json // or ResponseType.JSON
+            ),
+      )
+      .then((response) => print(response))
+      .catchError((error) => print(error));
+}
