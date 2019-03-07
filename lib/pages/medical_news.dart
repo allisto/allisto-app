9491145656b5b62eb864a63c253fba5e09da4a14
Rishi Banerjee <rishieric91@gmail.com>
2019-03-07 15:14:53 +0530

    Added the Medical Assistant and the Medical News Page

---
+++ lib/pages/medical_news.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedicalNews extends StatefulWidget {
+  @override
+  _MedicalNewsState createState() => _MedicalNewsState();
+}
+
+class _MedicalNewsState extends State<MedicalNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Medical News"),
+    );
+  }
+}
