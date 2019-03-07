6a5f7bf43bf0f45b570c96617d6aeba53d8444ec
yashovardhan99 <yashovardhandhanania@outlook.in>
2019-03-08 00:55:01 +0530

    Tried adding animation (Not working) and Post request (HTTP 500?)

--- lib/utils/upload_audio_to_server.dart
+++ lib/utils/upload_audio_to_server.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
+
 import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 
 void readAudioFile(String filePath, String apiUrl) async {
@@ -8,9 +8,9 @@ void readAudioFile(String filePath, String apiUrl) async {
 
   var audioFile = await config.readAsBytes();
   File _audioFile = new File(filePath); // TODO
+  print(audioFile.toString());
   Dio dio = Dio();
-  FormData formData = FormData(); // just like JS
-  formData.add("audio", UploadFileInfo(_audioFile, basename(filePath)));
+  FormData formData = new FormData.from(<String, dynamic>{"audio": UploadFileInfo(_audioFile, basename(filePath))});
   dio
       .post(
         apiUrl,
