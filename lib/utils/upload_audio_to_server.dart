5a1e8abb9568d523b74158e327b92e0dfceecea2
yashovardhan99 <yashovardhandhanania@outlook.in>
2019-03-08 19:47:14 +0530

    Got the POST request working

--- lib/utils/upload_audio_to_server.dart
+++ lib/utils/upload_audio_to_server.dart
@@ -7,19 +7,31 @@ void readAudioFile(String filePath, String apiUrl) async {
   var config = File(filePath);
 
   var audioFile = await config.readAsBytes();
-  File _audioFile = new File(filePath); // TODO
+  File _audioFile = new File(filePath);
   print(audioFile.toString());
+  print(_audioFile);
   Dio dio = Dio();
-  FormData formData = new FormData.from(<String, dynamic>{"audio": UploadFileInfo(_audioFile, basename(filePath))});
-  dio
-      .post(
-        apiUrl,
+  FormData formData = new FormData.from(
+      <String, dynamic>{
+        "audio_file": UploadFileInfo(_audioFile, basename(filePath))
+      });
+  print("Form data : " + formData.keys.toString());
+  dio.options.baseUrl = apiUrl;
+  dio.post("/audio/",
         data: formData,
         options: Options(
             method: 'POST',
+            connectTimeout: 10000,
+            contentType: ContentType.parse("application/json"),
+            receiveDataWhenStatusError: true,
+            receiveTimeout: 10000,
             responseType: ResponseType.json // or ResponseType.JSON
             ),
-      )
-      .then((response) => print(response))
-      .catchError((error) => print(error));
+    onSendProgress: (received, total) {
+      if (total != -1) {
+        print((received / total * 100).toStringAsFixed(0) + "%");
+      }
+    },)
+      .then((response) => print("Response: " + response.toString()))
+      .catchError((error) => print("Error : " + error.toString()));
 }
