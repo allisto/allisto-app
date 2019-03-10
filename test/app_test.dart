09fe9198c13dc27a064e4a5e79b49c9cccbcc0c5
yashovardhan99 <yashovardhandhanania@outlook.in>
2019-03-10 19:30:49 +0530

    Added new widget tests for home screen and renamed and grouped for readibility

--- test/app_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:allisto_app/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('app should work', (tester) async {
-    await tester.pumpWidget(new Allisto());
-    expect(find.text('Allisto'), findsOneWidget);
-  });
-}
